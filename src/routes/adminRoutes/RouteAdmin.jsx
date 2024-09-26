bd08c28b04c1a610d4ff1a1a3094da1c75a04632
vrindavnair <83174360+vrindavnair@users.noreply.github.com>
2024-09-26 10:50:26 +0530

    AddCategory and EditCategory pages

--- src/routes/adminRoutes/RouteAdmin.jsx
+++ src/routes/adminRoutes/RouteAdmin.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import AdminLayout from '../../layouts/AdminLayout'
 import UserManagement from '../../pages/adminPages/managementPages/UserManagement'
+import AddCategory from '../../components/CategorySubCategory/AddEditCategory/AddCategory'
+import EditCategory from '../../components/CategorySubCategory/AddEditCategory/EditCategory'
 
 function RouteAdmin() {
 
@@ -10,10 +12,14 @@ function RouteAdmin() {
   return (
     <Routes>
       <Route element={<AdminLayout/>}>
-        <Route path='usermanagement' element={<UserManagement/>}/>
+        {/* <Route path='usermanagement' element={<UserManagement/>}/> */}
            
-      <Route path='/userdetails' element={<UserDetails/>}/>
-      <Route path='/signIn'element={<SignInForm/>}/>
+      {/* <Route path='/userdetails' element={<UserDetails/>}/> */}
+      {/* <Route path='/signIn'element={<SignInForm/>}/> */}
+
+      <Route path='AddCategory' element={<AddCategory/>}/>
+       <Route path='EditCategory' element={<EditCategory/>}/>
+
       </Route>
     </Routes>
 
