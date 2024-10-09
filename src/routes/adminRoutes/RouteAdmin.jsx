38fc863708ee6e536d65e1a4961041c237b93103
JaseelaAlimahamed <jessibrototype@gmail,com>
2024-10-09 23:40:10 +0530

    feat :Code Updated

--- src/routes/adminRoutes/RouteAdmin.jsx
+++ src/routes/adminRoutes/RouteAdmin.jsx
@@ -1,17 +1,17 @@
 
 
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route ,Navigate} from 'react-router-dom'
 import AdminLayout from '../../layouts/AdminLayout.jsx'
 import UserManagement from '../../pages/adminPages/managementPages/UserManagement.jsx'
 import AdminProfilePage from '../../pages/adminPages/AdminProfilePage.jsx'
-import BookingDetailsPage from '../../pages/BookingDetailsPage.jsx'
-
-import AddCategory from '../../components/CategorySubCategory/AddEditCategory/AddCategory'
-import EditCategory from '../../components/CategorySubCategory/AddEditCategory/EditCategory'
-import FranchiseeProfile from '../../components/FranchiseComponents/FranchiseDetails/FranchiseeProfile'
+import BookingDetailsPage from '../../pages/adminPages/BookingDetailsPage.jsx'
+import SignInForm from '../../pages/adminPages/SignInPages/SignInForm.jsx'
+import AddCategory from '../../components/adminComponents/CategorySubCategory/AddEditCategory/AddCategory.jsx'
+import EditCategory from '../../components/adminComponents/CategorySubCategory/AddEditCategory/EditCategory.jsx'
+import FranchiseeProfile from '../../components/adminComponents/FranchiseDetails/FranchiseeProfile.jsx'
 import EditSubCategory from '../../components/adminComponents/CategorySubCategory/AddEditSubCategory/EditSubCategory.jsx'
-import AddSubCategory  from '../../components/adminComponents/CategorySubCategory/AddEditSubCategory/AddSubCategory.jsx'
+import AddSubCategory from '../../components/adminComponents/CategorySubCategory/AddEditSubCategory/AddSubCategory'
 
 import ServiceType from '../../pages/adminPages/ServiceType.jsx'
 
@@ -28,33 +28,24 @@ function RouteAdmin() {
 
   return (
     <Routes>
-
-      <Route path='profile' element={<AdminProfilePage/>}/>
-      <Route path='bookingdetails' element={<BookingDetailsPage/>}/>
-      
-      <Route element={<AdminLayout/>}>
-
-        <Route path='usermanagement' element={<UserManagement/>}/>
-
-           
      
-       <Route path='/servicetype' element={<ServiceType/>}/>
+      <Route path="/" element={<Navigate to="/admin/signIn"/>} />
+      <Route path='/signIn' element={<SignInForm />} />
 
-         <Route path='/userdetails' element={<UserDetails/>}/>
-         <Route path='/signIn'element={<SignInForm/>}/>
-         <Route path='AddCategory' element={<AddCategory/>}/>
-         <Route path='EditCategory' element={<EditCategory/>}/>
-         <Route path='FranchiseeDetails' element={<FranchiseeProfile/>}/>
-         <Route path='editsubcategory' element={<EditSubCategory/>}/>
-         <Route path='addsubcategory' element={<AddSubCategory/>}/>
-
-      <Route path='categories' element={<CategoryPage/>}/>
-      <Route path='subcategories' element={<SubCategoryPage/>}/>
-      <Route path='newads' element={<NewAdsPage/>}/>
-      {/* <Route path='/signIn'element={<SignInForm/>}/> */}
-
-
-
+      <Route element={<AdminLayout />}>
+        <Route path='/profile' element={<AdminProfilePage />} />
+        <Route path='/bookingdetails' element={<BookingDetailsPage />} />
+        <Route path='/usermanagement' element={<UserManagement />} />
+        <Route path='/servicetype' element={<ServiceType />} />
+        <Route path='/userdetails' element={<UserDetails />} />
+        <Route path='/AddCategory' element={<AddCategory />} />
+        <Route path='/EditCategory' element={<EditCategory />} />
+        <Route path='/FranchiseeDetails' element={<FranchiseeProfile />} />
+        <Route path='/editsubcategory' element={<EditSubCategory />} />
+        <Route path='/addsubcategory' element={<AddSubCategory />} />
+        <Route path='/categories' element={<CategoryPage />} />
+        <Route path='/subcategories' element={<SubCategoryPage />} />
+        <Route path='/newads' element={<NewAdsPage />} />
       </Route>
     </Routes>
 
