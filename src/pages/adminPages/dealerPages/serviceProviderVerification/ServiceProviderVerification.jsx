58c25b6a3b9722af9fc392c5633fd73ffab02cf9
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-10-10 20:36:01 +0530

    Service provider profile update

---
+++ src/pages/adminPages/dealerPages/serviceProviderVerification/ServiceProviderVerification.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ServiceProviderProfile from "../../../../components/dealerComponents/ServiceProviderVerification/ServiceProviderProfile"
+
+const ServiceProviderVerification = () => {
+  return (
+    <div>
+        <ServiceProviderProfile 
+            name="Karen Hope"
+            username="Plumber"
+            franchise="K Top"
+            address="Jakartra,Indonesia"
+            phone="+12 34567 890"
+            email="hope@gmail.com"
+  
+        />
+    </div>
+  )
+}
+
+export default ServiceProviderVerification
