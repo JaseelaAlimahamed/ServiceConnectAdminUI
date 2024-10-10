033802bbf06c5759e5ddc4efb18cfd733948cdab
Athul Nath <tkathulnath@gmail.com>
2024-10-11 00:53:19 +0530

    initial commit input

---
+++ src/components/franchiseComponent/inputComponent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const InputComponent = ({
+    label,
+    type,
+    name,
+    value,
+    onChange,
+    placeholder,
+    required,
+    className,
+}) => {
+    return (
+        <div>
+            {label &&(
+                <label htmlFor={name} className={className}>
+                    {label}
+                </label>
+            )}{' '}
+            <input
+                type = {type}
+                name = {name}
+                value = {value}
+                onChange = {onChange}
+                placeholder = {placeholder}
+                required = {required}
+                className = {className}
+            />
+        </div>
+    )
+}
+  
+export default InputComponent;
