9e922544600826aa789c9a1c5b86927f977cd18d
Athul Nath <tkathulnath@gmail.com>
2024-10-14 00:25:18 +0530

    initial commit add service provider page

--- src/components/franchiseComponent/addServiceProvider.jsx
+++ src/components/franchiseComponent/addServiceProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import FormComponent from "./formComponent";
-import InputComponent from "./inputComponent";
+import ButtonComponent from "./buttonComponent";
 
 
 function AddServiceProvider(){
@@ -11,35 +11,35 @@ function AddServiceProvider(){
             placeholder : 'Full Name',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]',
         },
         {
             name : 'address',
             placeholder : 'Address',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl flex placeholder:text-[#505050]',
         },
         {
             name : 'dob',
             type : 'date',
             placeholder : 'Date of Birth',
             required : true,
-            className : 'my-3 p-3 w-full  bg-white border-none rounded-xl flex text-[#A098AE] focus:placeholder',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl flex text-[#505050] focus:placeholder',
         },
         {
             name : 'email',
             placeholder : 'Email',
             type : 'email',
             required : true,
-            className : 'my-3 p-3 w-full block bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full block bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]',
         },
         {
             name : 'mobile',
             placeholder : 'Mobile number',
             type : 'tel',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]',
         },
         {
             name : 'franchiseename',
@@ -47,14 +47,14 @@ function AddServiceProvider(){
             type : 'dropdown',
             option : ['franchisee name 1','franchisee name 2','franchisee name 3'],
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex text-[#A098AE] list-item'
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex text-[#505050] list-item'
         },
         {
             name : 'password',
             placeholder : 'Password',
             type : 'password',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]'
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]'
         },
         {
             name : 'gender',
@@ -62,68 +62,85 @@ function AddServiceProvider(){
             type : 'dropdown',
             option : ['Male','Female'],
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex text-[#505050]',
         },
         {
             name : 'housename',
             placeholder : 'House name',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]',
         },
         {
             name : 'landmark',
             placeholder : 'Landmark',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE] ',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050] ',
         },
         {
             name : 'pincode',
             placeholder : 'Pincode',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]',
         },
         {
             name : 'district',
             placeholder : 'District',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE] ',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050] ',
         },
         {
             name : 'state',
             placeholder : 'State',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]',
         },
         {
             name : 'verificationId',
             placeholder : 'Verification ID',
             type : 'dropdown',
-            option : ['Aadhar','Voter-ID','Passport','Pan-card'],
+            option : ['Voter-ID','Passport','Pan-card','License'],
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex text-[#A098AE]',
+            className : 'my-3 p-2 h-10 h-10 w-full bg-white border-none rounded-xl text-justify flex text-[#505050]',
         },
         {
             name : 'verificationNo',
             placeholder : 'Verification Number',
             type : 'text',
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex placeholder:text-[#505050]',
         },
         {
             name : 'type',
             placeholder : 'Type',
             type : 'dropdown',
-            option : ['type 1','type 2','type 3','type 4'],
+            option : ['Cleaning','Repairing','Electrical','Masonry'],
             required : true,
-            className : 'my-3 p-3 w-full bg-white border-none rounded-xl text-justify flex text-[#A098AE]',
+            className : 'my-3 p-2 h-10 w-full bg-white border-none rounded-xl text-justify flex text-[#505050]',
         },
     ];
 
+    const buttonConfig = [
+        {
+            type : 'button',
+            children : 'Delete',
+            className : 'my-6  h-12 px-8 py-2 flex bg-[#E20000] border-2 text-white text-md rounded-3xl lg:w-18 hover:bg-white hover:text-[#E20000]',
+        },
+        {
+            type : 'submit',
+            children : 'Save as Draft',
+            className : 'my-4  h-12 px-4 py-2 flex bg-white border-2 text-[#4D44B5] rounded-3xl text-md sm:text-md hover:bg-[#4D44B5] hover:text-white',
+        },
+        {
+            type : 'submit',
+            children : 'Submit',
+            className : 'my-4  h-12 px-4 py-2 flex bg-[#4D44B5] border-2 text-white text-md rounded-3xl hover:bg-white hover:text-[#4D44B5]',
+        },
+    ];
     const [formData, setFormData] = useState({
         photo:'',
         fullname:'',
@@ -157,7 +174,8 @@ function AddServiceProvider(){
     };
 
     return(
-            <div className=" h-max m-2 bg-lite_blue2 rounded-b-md shadow-md">
+        <>
+            <div className=" h-full m-2 bg-lite_blue2 rounded-b-md shadow-md">
 
                 <div className=" h-1/6 bg-[#4D44B5] rounded-t-md">
                     <h1 className="text-primary text-2xl py-2 px-3 text-justify m-auto">Service Provider Details</h1>
@@ -165,10 +183,10 @@ function AddServiceProvider(){
 
                 <div className="justify-start sm:flex grid">
 
-                    <div className="w-auto h-52 mx-8 mt-8 grid sm:w-48" onClick={handleImageClick}>
+                    <div className="w-fit h-52 mx-8 mt-8 grid sm:mx-8 cursor-pointer" onClick={handleImageClick}>
                         <h1 className="text-dark_blue text-lg font-semibold">Photo *</h1>
 
-                        <div className="bg-primary w-40 h-40 flex justify-center items-center rounded-md border border-[#A098AE] border-dashed cursor-pointer">
+                        <div className="bg-primary w-40 h-40 p-2 flex justify-center items-center rounded-md border-2 border-[#C1BBEB] border-dashed">
                             <p className="text-center text-[#A098AE] text-sm">Drag and drop or click here to select file</p>
                         </div>
 
@@ -176,7 +194,7 @@ function AddServiceProvider(){
                         
                     </div>
 
-                    <div className="px-4 mt-8 w-80 sm:w-96 sm:px-8">
+                    <div className="mx-4 mt-8 w-80 sm:w-96 sm:px-auto md:px-auto">
                         <FormComponent
                             fieldConfig = {fieldConfig}
                             onSubmit = {HandleSubmit}
@@ -185,7 +203,21 @@ function AddServiceProvider(){
                         <p className="font-semibold text-sm flex justify-center p-2">#Other info as required</p>
                     </div> 
                 </div>
+
             </div>
+            <div className=" flex gap-2 justify-center items-center mx-2 my-2 sm:mx-12 sm:gap-8 sm:justify-end">
+                {
+                    buttonConfig.map((btn) => (
+                        <ButtonComponent
+                            key = {btn.key}
+                            type = {btn.type}
+                            children = {btn.children}
+                            className = {btn.className}
+                        />
+                    ))
+                }
+            </div>
+        </>
     )
 };
 export default AddServiceProvider;
