8e7ee50490fd6766420a98ec9532ced1f4a7a1e1
Nabban muneer pp <nabbanmuneerpp@gmail.com>
2024-10-14 09:40:28 +0530

    complete the dealer routes

--- src/routes/dealerRoutes/RouteDealer.jsx
+++ src/routes/dealerRoutes/RouteDealer.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import NavBar from '../../components/dealerComponents/layoutComponents/NavBar'
-import SideBar from '../../components/dealerComponents/layoutComponents/SideBar'
 import DealerLayout from '../../layouts/DealerLayout'
+import Dashboard from '../../pages/dealerPages/Dashboard'
+import ServieProviderManagment from '../../pages/dealerPages/ServieProviderManagment'
+import ServieProvider from '../../pages/dealerPages/ServieProvider'
+import ServieProviderVarication from '../../pages/dealerPages/ServieProviderVaricationForm'
+import PaymentRequestForm from '../../pages/dealerPages/PaymentRequestForm'
 
 export default function RouteDealer() {
   return (
-    <Routes >
-        <Route path='/' element={<SideBar/>} />
+    <Routes element={<DealerLayout />} >
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/ServiesProviderManagment' element={<ServieProviderManagment />} />
+        <Route path='/ServiesProviderVerication' element={<ServieProvider />} />
+        <Route path='/ServiesProviderVaricationForm' element={<ServieProviderVarication />} />
+        <Route path='/PaymentRequestForm' element={<PaymentRequestForm />} />
     </Routes>
   )
 }
