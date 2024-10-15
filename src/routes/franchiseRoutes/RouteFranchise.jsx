47120ffae4e57f66874a05b9cd0cc06d8c72bbf6
Akash <akashthomas126@gmail.com>
2024-10-16 02:44:36 +0530

    Service Providers List page added on Franchise Component

--- src/routes/franchiseRoutes/RouteFranchise.jsx
+++ src/routes/franchiseRoutes/RouteFranchise.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import DealersPage from '../../pages/franchisePages/DealersPage'
+import ServiceProviders from '../../components/franchiseComponents/serviceProviders/ServiceProviders'
 
 function RouteFranchise() {
   return (
     <Routes>
         <Route path='dealers' element ={<DealersPage/>} ></Route>
+        <Route path='service-providers' element={<ServiceProviders/>}/>
     </Routes>
   )
 }
