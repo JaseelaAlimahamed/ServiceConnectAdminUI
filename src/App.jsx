232d5fd4619a3d0d977d6fae3db74122ee140695
“[bijitha <[bijithawilson98@gmail.com]>
2024-10-17 18:45:55 +0530

    Created add dealer page and signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import RouteAdmin from './routes/adminRoutes/RouteAdmin';
+import RouteAdmin from './routes/adminRoutes/RouteAdmin'
 import AddNewDealer from './components/FranchiseComponents/dealerComponents/Add_Dealer'
 import AddNewUser from './components/adminComponents/AddNewUser'
+import SignInPage from './components/franchiseComponents/pages/Sign_In'
 
 
 
@@ -16,9 +17,11 @@ function App() {
    
     
          <Routes>
-        //   <Route path="/admin/*" element={<RouteAdmin/>} />
+        //<Route path="/admin/*" element={<RouteAdmin/>} />
         <Route path='/Add_Dealer' element={<AddNewDealer/>}/>
-        <Route path='/AddNewUser' element={<AddNewUser/>}/>
+       
+        <Route path='/SignInPage' element={<SignInPage/>}/>
+    
     
          </Routes>
        
--- src/components/FranchiseComponents/pages/Sign_In.jsx
+++ src/components/FranchiseComponents/pages/Sign_In.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import logo from "/LOGO.png";
-import InputField from '../ReUsableComponents/InputField';
-import SubmitButton from '../ReUsableComponents/SubmitButton';
+import InputFieldComponent from '../../ReUsableComponents/InputFieldComponent';
+import SubmitButton from '../../ReUsableComponents/SubmitButton';
+
+
+
 
 const SignInPage = () => {
     
@@ -15,40 +18,40 @@ const SignInPage = () => {
   };
 
   return (
-    <div className='bg-primary h-screen text-black flex flex-col justify-center items-center px-4'>
+    <div className='bg-primary h-screen text-black flex flex-col justify-center items-center px-4 border-2 border-gray-900' >
+      
       <div className='mt-5 text-center flex'>
         <img src={logo} alt="logo" className='w-20 h-24 mx-auto me-3' />
         <h1 className='text-2xl font-bold mt-2 me-3'>Service Connect</h1>
       </div>
 
       <form className='flex flex-col gap-4 w-full max-w-sm font-default' onSubmit={handleSubmit}>
-        <h2 className='text-xl font-semibold ml-3'>Login to your account</h2>
-        
-        <InputField
+        <h2 className='text-xl font-semibold ml-3'>Welcome to your Franchise account</h2>
+        <InputFieldComponent
           type="email"
-          placeholder="Email"
+          placeholder=" &#x2709; Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          color='bg-custom_gray placeholder-white text-white'
         />
-
-        <InputField
-          type="password"
-          placeholder="Password"
+        <InputFieldComponent
+          type={showPassword ? "text" : "password"}
+          placeholder="&#x1F512; Password "
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           isPassword={true}
           showPassword={showPassword}
           toggleShowPassword={() => setShowPassword(!showPassword)}
+          color='bg-custom_gray border-2 border-gray-900  placeholder-white text-white'
         />
-
         <div className='flex justify-between p-2'>
-          <p>
+          <p className='hover:text-blue-700'>
             <input type="checkbox" /> Remember Me
           </p>
-          <a href="">Forgot Password...!</a>
+          <a href="" className='hover:text-blue-700'>Forgot Password !</a>
         </div>
-
         <SubmitButton text="Sign In" />
+
       </form>
 
     </div>
