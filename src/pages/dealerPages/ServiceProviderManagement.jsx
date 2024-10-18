f16aa1be4fb0558d35800bc2df9ac636c9a52f66
Alfin-anto <alfinanto098@gmail.com>
2024-10-18 10:46:16 +0530

    dealerComponents/DealerTable

--- src/pages/dealerPages/ServiceProviderManagement.jsx
+++ src/pages/dealerPages/ServiceProviderManagement.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Table from '../../components/DealerComponents/DealerTable'
+import Table from '../../components/dealerComponents/DealerTable'
 
 const Serviceprovidermanagement = () => {
 
