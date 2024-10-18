d7341619d9aef42e3d98485ba3456ad26dba564c
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-10-18 13:21:30 +0530

    verification component folder

--- src/pages/dealerPages/ServiceProviderVerification.jsx
+++ src/pages/dealerPages/ServiceProviderVerification.jsx
@@ -1,6 +1,6 @@
-import ServiceProviderProfile from "../../components/dealerComponents/serviceProviderVerification/ServiceProviderProfile";
-import AnalyticsComponent from "../../components/dealerComponents/serviceProviderVerification/AnalyticsComponent";
-import DealerButton from "../../components/dealerComponents/serviceProviderVerification/DealerButton";
+import ServiceProviderProfile from "../../components/dealerComponents/verificationComponents/ServiceProviderProfile";
+import AnalyticsComponent from "../../components/dealerComponents/verificationComponents/AnalyticsComponent";
+import DealerButton from "../../components/dealerComponents/verificationComponents/DealerButton";
 
 const ServiceProviderVerification = () => {
   return (
