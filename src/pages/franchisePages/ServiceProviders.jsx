6c2257c4b0c6824c32e127d36d1d191fa94d73f3
nithinmjojo <nithinmjojo@gmail.com>
2024-10-29 19:51:50 +0530

    feat:Added Service Provider table in Franchise

---
+++ src/pages/franchisePages/ServiceProviders.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServiceProviderList from '../../components/franchiseComponents/serviceProviderComponents/ServiceProviderList'
+
+const ServiceProviders = () => {
+  return (
+    <div>
+      <ServiceProviderList/>
+    </div>
+  )
+}
+
+export default ServiceProviders
