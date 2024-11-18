23fe583fd39a42933e954a1884d608eeb4ea5d22
vishnurptly <vishnurptly@gmail.com>
2024-11-18 11:56:45 +0530

    category add and delete

--- src/service/api/admin/GetApi.js
+++ src/service/api/admin/GetApi.js
@@ -1,3 +1,19 @@
 import { apiInstance } from "../ApiInstence";
 
 
+
+export const categoryListGet = async () => {
+    try {
+      const response = await apiInstance.get("categories/");
+    
+       const list = response.data
+      
+      return list;
+    } catch (error) {
+      console.error(error);
+      throw error.response ? error.response.data : new Error(error.message);
+    }
+  };
+
+  
+
