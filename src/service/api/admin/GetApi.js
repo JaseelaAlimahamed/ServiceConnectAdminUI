2f3dea6113267262e97b4f0ae6983b5b524b001c
fadhilhussain 12 <fadhilhussain12@gmail.com>
2024-11-18 15:16:12 +0530

    franchise cards

--- src/service/api/admin/GetApi.js
+++ src/service/api/admin/GetApi.js
@@ -1,3 +1,30 @@
 import { apiInstance } from "../ApiInstence";
 
+/**
+ * Fetch franchisee statistics
+ * @returns {Promise<object>} - Franchisee statistics data
+ */
+export const getFranchiseeStats = async () => {
+    try {
+        const response = await apiInstance.get("/franchisee-stats/");
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error.response ? error.response.data : new Error(error.message);
+    }
+};
 
+/**
+ * Fetch Active Franchisee Stats
+ * @returns {Promise<Object>} - API response
+ */
+export const getActiveFranchiseeStats = async () => {
+    try {
+      const response = await apiInstance.get('/franchisee-active-stats/');
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching active franchisee stats:", error);
+      throw error.response ? error.response.data : new Error(error.message);
+    }
+  };
+  
