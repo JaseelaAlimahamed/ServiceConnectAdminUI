4e2a00b05bf303a275f466ae89d9806638e4d0b6
akshaygoal <akshaychikku262@gmail.com>
2024-11-19 13:26:42 +0530

    profile
    updated

--- src/components/adminComponents/adsManagement/AdsCards.jsx
+++ src/components/adminComponents/adsManagement/AdsCards.jsx
@@ -1,108 +1,45 @@
-import React from "react";
-import Card from "../../reUsableComponents/Card";
-import { fetchAdData } from "../../../service/api/admin/AdsApi";
-import { useState, useEffect } from "react";
+import React from 'react'
+import Card from '../../ReUsableComponents/Card'
 
 function AdsCards() {
-  const [data, setData] = useState();
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetchAdData();
-      setData(response.data);
-    };
-    fetchData();
-  }, []);
-
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4 sm:gap-4 md:gap-2 w-full px-4 justify-items-center sm:justify-items-stretch mb-4">
-        {data && (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4 sm:gap-4 md:gap-2 w-full px-4 justify-items-center sm:justify-items-stretch mb-4">
           <Card
             variant="ads"
             heading="Total Ads"
-            title={data.total_ads.toString()}
+            title="932"
             subtitle=" than last month"
             percent="+10%"
-            iconBg="bg-dark_blue"  
+            iconBg="bg-dark_blue"
           />
-        )}
-        {data && (
           <Card
             variant="ads"
             heading="Total Ads Revenue"
-            title={data.total_revenue.toString()}
+            title="932"
             subtitle=" than last month"
             percent="-0.5%"
             iconBg="bg-orange"
           />
-        )}
-
-        <Card
-          variant="ads"
-          heading="Boosted Profile"
-          title="932"
-          subtitle=" than last month"
-          percent="-0.6%"
-          iconBg="bg-dark_blue"
-        />
-        <Card
-          variant="ads"
-          heading="Other Ads"
-          title="754"
-          subtitle=" than last month"
-          percent="-0.6%"
-          iconBg="bg-orange"
-        />
-      </div>
+          <Card
+            variant="ads"
+            heading="Boosted Profile"
+            title="932"
+            subtitle=" than last month"
+            percent="-0.6%"
+            iconBg="bg-dark_blue"
+          />
+          <Card
+            variant="ads"
+            heading="Other Ads"
+            title="754"
+            subtitle=" than last month"
+            percent="-0.6%"
+            iconBg="bg-orange"
+          />
+        </div>
     </>
-  );
+  )
 }
 
-export default AdsCards;
-
-
-// import React from 'react'
-// import Card from '../../ReUsableComponents/Card'
-
-// function AdsCards() {
-//   return (
-//     <>
-//     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4 sm:gap-4 md:gap-2 w-full px-4 justify-items-center sm:justify-items-stretch mb-4">
-//           <Card
-//             variant="ads"
-//             heading="Total Ads"
-//             title="932"
-//             subtitle=" than last month"
-//             percent="+10%"
-//             iconBg="bg-dark_blue"
-//           />
-//           <Card
-//             variant="ads"
-//             heading="Total Ads Revenue"
-//             title="932"
-//             subtitle=" than last month"
-//             percent="-0.5%"
-//             iconBg="bg-orange"
-//           />
-//           <Card
-//             variant="ads"
-//             heading="Boosted Profile"
-//             title="932"
-//             subtitle=" than last month"
-//             percent="-0.6%"
-//             iconBg="bg-dark_blue"
-//           />
-//           <Card
-//             variant="ads"
-//             heading="Other Ads"
-//             title="754"
-//             subtitle=" than last month"
-//             percent="-0.6%"
-//             iconBg="bg-orange"
-//           />
-//         </div>
-//     </>
-//   )
-// }
-
-// export default AdsCards
+export default AdsCards
