1ed099d6b7e1f654b2cf6a4233a9decc05a5e367
fahiz <fahizk100@gmail.com>
2024-11-19 17:09:10 +0530

    error manage

--- src/service/api/admin/GetApi.js
+++ src/service/api/admin/GetApi.js
@@ -14,9 +14,6 @@ export const getFranchiseeStats = async () => {
   }
 };
 
-export const categoryListGet = async () => {
-  try {
-    const response = await apiInstance.get("categories/");
 
 //categoryList api 
 export const categoryListGet = async () => {
@@ -32,14 +29,7 @@ export const categoryListGet = async () => {
     }
   }
 
-    const list = response.data;
-
-    return list;
-  } catch (error) {
-    console.error("Error fetching active franchisee stats:", error);
-    throw error.response ? error.response.data : new Error(error.message);
-  }
-};
+   
 
 /**
  * Fetch Active Franchisee Stats
@@ -67,29 +57,8 @@ export const listUser = async () => {
     }
   };
 
-  // total customer
-  export const totalCustomer = async () => {
-    try {
-      const response = await apiInstance.get("total-customer/");
-      console.log(response);
-      return response.data.total_customers;
-    } catch (error) {
-      console.error(error);
-      throw error.response ? error.response.data : new Error(error.message);
-    }
-  };
 
-   // total service request
-   export const totalServiceRequest = async () => {
-    try {
-      const response = await apiInstance.get("total-servicerequest/");
-      console.log(response);
-      return response.data.total_service_requests_count;
-    } catch (error) {
-      console.error(error);
-      throw error.response ? error.response.data : new Error(error.message);
-    }
-  };
+
 
   //SubCategoryList Api
   export const SubcategoryGet = async () => {
@@ -118,62 +87,6 @@ export const listUser = async () => {
 
 
 
-  // online customers
-  export const onlineCustomers = async () => {
-    try {
-      const response = await apiInstance.get("online-customers/");
-      console.log(response);
-      return response.data.online_customers_count;
-    } catch (error) {
-      console.error(error);
-      throw error.response ? error.response.data : new Error(error.message);
-    }
-  };
-
-  // lead service requestcount
-  export const leadServiceRequestCount = async () => {
-    try {
-      const response = await apiInstance.get("lead-service-request-count/");
-      console.log(response);
-      return response.data.lead_service_request_count;
-    } catch (error) {
-      console.error(error);
-      throw error.response ? error.response.data : new Error(error.message);
-    }
-  };
-
-   // active service request count
-   export const activeServiceRequestCount = async () => {
-    try {
-      const response = await apiInstance.get("active-service-request-count/");
-      console.log(response);
-      return response.data.active_service_request_count;
-    } catch (error) {
-      console.error(error);
-      throw error.response ? error.response.data : new Error(error.message);
-    }
-  };
-
-   // complaintsTotal
-   export const complaintsTotal = async () => {
-    try {
-      const response = await apiInstance.get("complaints-total/");
-      console.log(response);
-      return response.data.total_complaints;
-    } catch (error) {
-      console.error(error);
-      throw error.response ? error.response.data : new Error(error.message);
-    }
-  };
-  try {
-    const response = await apiInstance.get("usersview/");
-    console.log(response);
-    return response.data.results;
-  } catch (error) {
-    console.error(error);
-    throw error.response ? error.response.data : new Error(error.message);
-  }
-};
 
 // total customer
 export const totalCustomer = async () => {
