5f71d25e1db97312972757342d93795773d31f61
fahiz <fahizk100@gmail.com>
2024-11-21 10:09:59 +0530

    add some admin page items

--- src/service/api/admin/PutApi.js
+++ src/service/api/admin/PutApi.js
@@ -1,3 +1,34 @@
 import { apiInstance } from "../ApiInstence";
 
 
+export const editProfile = async (data) => {
+    try {
+      const response = await apiInstance.patch("profile/", data);
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error.response ? error.response.data : new Error(error.message);
+    }
+  };
+
+//SubCategory Edit Api
+export const SubcategoryEdit = async (id, data) => {
+  try {
+    // Log FormData before sending
+    console.log("Data sent to API:");
+    for (const [key, value] of data.entries()) {
+      console.log(`${key}:`, value);
+    }
+
+    const response = await apiInstance.put(`subcategories/${id}/`, data, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    console.log("Response:", response.data);
+    return response;
+  } catch (error) {
+    console.error("Error editing sub-category:", error.response?.data || error.message);
+    throw error.response ? error.response.data : new Error(error.message);
+  }
+};
