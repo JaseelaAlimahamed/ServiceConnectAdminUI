3a80c53f81099dfb6b0381d1fb76139db797961e
fahiz <fahizk100@gmail.com>
2024-11-21 10:30:39 +0530

    review fadhi code

--- src/service/api/admin/GetApi.js
+++ src/service/api/admin/GetApi.js
@@ -202,3 +202,30 @@ export const getFranchiseeTypes = async () => {
   }
 };
 
+
+
+/**
+ * Fetch Active Franchisee Stats
+ * @returns {Promise<Object>} - API response
+ */
+export const getInActiveFranchiseeStats = async () => {
+  try {
+    const response = await apiInstance.get("/franchisee-inactive-stats/");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching inactive franchisee stats:", error);
+    throw error.response ? error.response.data : new Error(error.message);
+  }
+};
+// Adds Management 
+
+export const fetchAdData = async () => {
+  try {
+    const response = await apiInstance.get("/ad_management/");
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching ads:", error);
+    throw error;
+  }
+};
